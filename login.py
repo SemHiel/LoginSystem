24e46365db92c70d74e0d2975394717060b7977c
sequos <sequos.contact@gmail.com>
2020-10-21 18:16:34 +0200

    first

---
+++ login.py
@@ -0,0 +1,35 @@
+users = {}
+status = ""
+
+
+def displayMenu():
+    status = input("Are you registered user? y/n? Press q to quit \n")
+    if status == "y":
+        oldUser()
+    elif status == "n":
+        newUser()
+
+
+def newUser():
+    createLogin = input("Create login name: ")
+
+    if createLogin in users:
+        print("\nLogin name already exist!\n")
+    else:
+        createPassw = input("Create password: ")
+        users[createLogin] = createPassw
+        print("\nUser created\n")
+
+
+def oldUser():
+    login = input("Enter login name: ")
+    passw = input("Enter password: ")
+
+    if login in users and users[login] == passw:
+        print("\nLogin successful!\n")
+    else:
+        print("\nUser doesn't exist or wrong password!\n")
+
+
+while status != "q":
+    displayMenu()
